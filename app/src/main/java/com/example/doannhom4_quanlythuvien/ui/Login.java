7998263196f0a67a6bd1bd0fefa0b689e5bf011a
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-11-23 14:55:56 +0700

    update UI Auth

--- app/src/main/java/com/example/doannhom4_quanlythuvien/ui/Login.java
+++ app/src/main/java/com/example/doannhom4_quanlythuvien/ui/Login.java
@@ -7,26 +7,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.doannhom4_quanlythuvien.R;
 import com.example.doannhom4_quanlythuvien.helpers.StaticConfig;
+import com.example.doannhom4_quanlythuvien.model.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
     private TextView link_register, forgotpass;
     private Button btnLogin;
     private ProgressBar progressBar;
     private EditText etEmail, etPass;
-
+    int RC_SIGN_IN = 73;
+    FirebaseAuth auth;
+    GoogleSignInClient mGoogleSignInClient;
+    ImageView btnGoogleLoginPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +52,21 @@ public class Login extends AppCompatActivity {
     }
 
     private void setEvent() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btnGoogleLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//                startActivityForResult(signInIntent, RC_SIGN_IN);
+                Toast.makeText(getApplicationContext(), "Todo", Toast.LENGTH_SHORT).show();
+            }
+        });
         forgotpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,6 +118,7 @@ public class Login extends AppCompatActivity {
         });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
                 String Email = etEmail.getText().toString();
                 String Pass = etPass.getText().toString();
@@ -113,6 +145,55 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            User user1 = new User();
+                            user1.setId(user.getUid());
+                            user1.setname(user.getDisplayName());
+                            user1.setPic(user.getPhotoUrl().toString());
+//                            database.getReference().child("Users").child(user.getUid()).setValue(user1);
+                            StaticConfig.mUser.child(user.getUid()).setValue(user1);
+                            Intent intent = new Intent(getApplicationContext(), Starup.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "Sign in With Google", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+//                            Snackbar.make(binding.getRoot(), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
     private boolean kiemtra() {
         if (etEmail.getText().toString().isEmpty()) {
             etEmail.setError("email is require!!");
@@ -139,5 +220,6 @@ public class Login extends AppCompatActivity {
         etEmail = findViewById(R.id.lemail);
         etPass = findViewById(R.id.lpass);
         progressBar = findViewById(R.id.progressBar);
+        btnGoogleLoginPage = findViewById(R.id.btnGoogleLoginPage);
     }
 }
